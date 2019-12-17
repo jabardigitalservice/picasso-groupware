00cbc2f79a787cdf5035dda2a4f79602552d6e2a
Yoga Hanggara <yohang88@gmail.com>
2019-12-18 00:55:21 +0700

    disable skip waiting

--- vue.config.js
+++ vue.config.js
@@ -3,9 +3,6 @@ module.exports = {
     name: 'Groupware',
     themeColor: '#399F4F',
     msTileColor: '#FFFFFF',
-    workboxOptions: {
-      skipWaiting: true
-    },
     manifestOptions: {
       background_color: '#399F4F'
     }
