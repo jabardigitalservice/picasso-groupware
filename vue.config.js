37aee77eb9c37b6f578f273c7db09c1666c70b18
Yoga Hanggara <yohang88@gmail.com>
2019-12-18 22:48:05 +0700

    icons icons

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,8 @@ module.exports = {
   pwa: {
     name: 'Groupware',
     themeColor: '#399F4F',
-    msTileColor: '#FFFFFF',
     manifestOptions: {
-      background_color: '#399F4F'
+      background_color: '#FFFFFF'
     }
   }
 }
