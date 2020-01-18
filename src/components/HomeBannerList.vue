eb94c5f0cf677d0148cac0b7cf54607357e5144d
Yoga Hanggara <yohang88@gmail.com>
2020-01-18 19:18:07 +0700

    banners height. loop

--- src/components/HomeBannerList.vue
+++ src/components/HomeBannerList.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="home-banner-list">
     <template v-if="!loading">
-      <carousel :autoplay="true" :autoplayTimeout="4000" :perPageCustom="[[768, 2], [1024, 4]]" :pagination-enabled="false" :per-page="1" :mouse-drag="false">
+      <carousel class="shadow sm:mx-2" :autoplay="true" :autoplayTimeout="4000" :perPageCustom="[[768, 2], [1024, 4]]" :pagination-enabled="false" :per-page="1" :mouse-drag="false" :loop="true">
         <slide v-for="item in items" :key="item.id" class="item">
-          <div class="mx-2 h-32 bg-cover bg-center rounded-lg shadow" :style="`background-image: url(${item.url})`"></div>
+          <img class="w-full" :src="item.url" />
         </slide>
       </carousel>
     </template>
     <template v-else>
-      <div class="mx-2 bg-white rounded-lg shadow p-4">
+      <div class="bg-white sm:mx-2 shadow p-4">
         <content-loader :speed="2">
           <rect x="0" y="0" rx="0" ry="0" width="100%" height="220" />
         </content-loader>
