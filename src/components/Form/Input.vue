dc661676761f120b9ded9b38bfb5ff95f68d66c9
adrianpdm <adrian.padmodihardjo@gmail.com>
2020-03-03 16:44:57 +0700

    create input elements for form

---
+++ src/components/Form/Input.vue
@@ -0,0 +1,63 @@
+<template>
+  <ValidationProvider :rules="rules"
+                      :custom-messages="customMessages"
+                      #default="{failed, errors}"
+                      tag="div">
+    <FormInputHeader :label-for="name"
+                      :title="title"
+                      :subtitle="subtitle">
+      <template #title>
+        <slot name="title"></slot>
+      </template>
+      <template #subtitle>
+        <slot name="subtitle"></slot>
+      </template>
+    </FormInputHeader>
+    <input ref="input"
+           :type="type"
+           :name="name"
+           :value="value"
+           :class="{'form-input__input': true, 'is-invalid': failed}"
+           v-bind="$attrs"
+           @input="onInput">
+    <p v-if="errors.length"
+       class="form-input__error-hint">
+      <slot name="error">
+        {{errors[0]}}
+      </slot>
+    </p>
+  </ValidationProvider>
+</template>
+
+<script>
+import { props, components } from './input-mixin'
+
+export default {
+  components: {
+    ...components
+  },
+  model: {
+    prop: 'value',
+    event: 'input'
+  },
+  props: {
+    ...props,
+    type: {
+      type: [String],
+      required: true
+    },
+    value: {
+      type: [String, Number],
+      default: ''
+    }
+  },
+  methods: {
+    onInput (e) {
+      this.$emit('input', e.target.value)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
