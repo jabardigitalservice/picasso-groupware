9c79e93ba701fecb1ebec909baeea1a46168d890
adrianpdm <adrian.padmodihardjo@gmail.com>
2020-03-05 23:13:53 +0700

    add warning before data change

--- src/components/Profile/Edit/Experience.vue
+++ src/components/Profile/Edit/Experience.vue
@@ -7,12 +7,15 @@
                   rules="required"
                   prompt="Pilih salah satu opsi di bawah ini"
                   :custom-messages="{required: 'Informasi ini harus diisi'}"
-                  v-model="mPreviousJobData.has_previous_job" />
+                  :value="mPreviousJobData.has_previous_job"
+                  @change="onChangePreviousJobStatus"/>
     </div>
     <transition name="slide-y-fade-transition">
       <div v-if="mPreviousJobData.has_previous_job === 'Ya'">
         <div class="form-input-container">
-          <FormInput :required="false"
+          <FormInput :required="isPreviousJobExist"
+                      :rules="requiredIfPreviousJobExist"
+                      :custom-messages="{required: 'Nama perusahaan harus diisi'}"
                       type="text"
                       name="latest_job_company"
                       title="Nama Perusahaan Terakhir"
@@ -20,7 +23,9 @@
                       v-model="mPreviousJobData.company" />
         </div>
         <div class="form-input-container">
-          <FormInput :required="false"
+          <FormInput :required="isPreviousJobExist"
+                      :rules="requiredIfPreviousJobExist"
+                      :custom-messages="{required: 'Nama posisi harus diisi'}"
                       type="text"
                       name="latest_job_position"
                       title="Posisi di Perusahaan Terakhir"
@@ -28,7 +33,9 @@
                       v-model="mPreviousJobData.position" />
         </div>
         <div class="form-input-container">
-          <FormInput :required="false"
+          <FormInput :required="isPreviousJobExist"
+                      :rules="requiredIfPreviousJobExist"
+                      :custom-messages="{required: 'Lama di posisi harus diisi'}"
                       type="text"
                       name="latest_job_length"
                       title="Lama Bekerja di Perusahaan Terakhir"
@@ -37,7 +44,9 @@
                       v-model="mPreviousJobData.length" />
         </div>
         <div class="form-input-container">
-          <FormInput :required="false"
+          <FormInput :required="isPreviousJobExist"
+                      :rules="requiredIfPreviousJobExist"
+                      :custom-messages="{required: 'Informasi ini harus diisi'}"
                       type="number"
                       name="latest_job_salary"
                       title="Gaji di Perusahaan Terakhir"
@@ -56,7 +65,7 @@
 </template>
 
 <script>
-import { PROFILE_DETAIL_TYPE, populateProfileDataFields, validateAndSave } from './utils'
+import { PROFILE_DETAIL_TYPE, profileObjectReference, populateProfileDataFields, validateAndSave } from './utils'
 
 export default {
   components: {
@@ -71,14 +80,54 @@ export default {
   data () {
     return {
       mPreviousJobData: {},
-
       hasSetRequiredFields: false
     }
   },
+  computed: {
+    isPreviousJobExist () {
+      return this.mPreviousJobData.has_previous_job === 'Ya'
+    },
+    requiredIfPreviousJobExist () {
+      return {
+        required: this.isPreviousJobExist
+      }
+    }
+  },
   methods: {
+    onChangePreviousJobStatus (newValue) {
+      const oldValue = this.mPreviousJobData.has_previous_job
+      const keys = profileObjectReference[PROFILE_DETAIL_TYPE.PREVIOUS_JOB]
+      const hasSomeValues = keys.some(key => {
+        if (key === 'has_previous_job') return false
+        return this.mPreviousJobData[key] !== null
+      })
+
+      if (newValue === 'Tidak' && oldValue === 'Ya' && hasSomeValues) {
+        this.$swal.fire({
+          title: 'Apa kamu yakin?',
+          text: 'Mengganti opsi ini akan menghapus data yang sudah kamu isikan sebelumnya',
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonText: 'Ya, hapus saja datanya',
+          cancelButtonText: 'Keluar'
+        }).then(({ value }) => {
+          if (value) {
+            keys.forEach(key => {
+              if (key === 'has_previous_job') return
+              this.$set(this.mPreviousJobData, key, null)
+            })
+            this.$set(this.mPreviousJobData, 'has_previous_job', newValue)
+          }
+        })
+      } else {
+        this.$set(this.mPreviousJobData, 'has_previous_job', newValue)
+      }
+    },
     onSave () {
       return validateAndSave(this.$refs.validator, this.data.id, {
         [PROFILE_DETAIL_TYPE.PREVIOUS_JOB]: this.mPreviousJobData
+      }).then(() => {
+        this.$emit('reload:profile')
       })
     }
   },
