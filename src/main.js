60f6557dbb3a8fc7b89fe09872461c161499ca09
adrianpdm <adrian.padmodihardjo@gmail.com>
2020-03-05 16:32:08 +0700

    assign vee validate as vue plugin

--- src/main.js
+++ src/main.js
@@ -6,11 +6,16 @@ import store from './store'
 import './lib/firebase'
 
 import '@fortawesome/fontawesome-free/js/all'
+import 'sweetalert2/src/sweetalert2.scss'
 
 // plugins
 import VeeValidate from './plugins/vee-validate'
+import SweetAlert2 from './plugins/sweet-alert2'
 
-Vue.use(VeeValidate)
+;[
+  VeeValidate,
+  SweetAlert2
+].forEach(plugin => Vue.use(plugin))
 
 Vue.config.productionTip = false
 
