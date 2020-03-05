3ae4066b796e27b05a76efb0141934461cc30934
adrianpdm <adrian.padmodihardjo@gmail.com>
2020-03-06 01:02:02 +0700

    add visual hint on data changed

--- src/components/Profile/Edit/ProfileSectionDetail.vue
+++ src/components/Profile/Edit/ProfileSectionDetail.vue
@@ -3,16 +3,18 @@
     <h3 class="p-8 font-bold text-2xl text-white bg-brand-blue">
       <transition name="slide-y-fade-transition"
                   mode="out-in">
-        <span :key="name">
+        <span :key="name" :class="['profile-section-detail__title', hasUnsavedChanges && 'has-unsaved-changes']">
           {{name}}
         </span>
       </transition>
     </h3>
     <transition name="slide-y-fade-transition"
                 mode="out-in">
-      <component :key="name"
+      <component ref="profileData"
+                 :key="name"
                  :is="sectionComponent"
                  :data="data"
+                 :unsaved="!hasUnsavedChanges"
                  class="p-8"
                  @reload:profile="onReloadProfile" />
     </transition>
@@ -37,6 +39,9 @@ export default {
         return () => import(`./${this.name.replace(' ', '')}.vue`)
       }
       return null
+    },
+    hasUnsavedChanges () {
+      return this.$store.state['profile-detail'].hasUnsavedChanges
     }
   },
   methods: {
@@ -51,5 +56,21 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.profile-section-detail__title {
+  @apply relative;
+  &::after {
+    content: '';
+    transform: scale(0);
+    transform-origin: center;
+    left: calc(100% + #{0.5rem});
+    transition: all 0.3s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+    @apply block absolute top-0 w-4 h-4 rounded-full bg-yellow-400;
+  }
+}
+.has-unsaved-changes {
+  &::after {
+    transform: scale(1);
+  }
+}
 </style>
