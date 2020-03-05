bb1bb04d5fa588454c93c7f5065325c4d13bf054
adrianpdm <adrian.padmodihardjo@gmail.com>
2020-03-05 16:35:39 +0700

    encapsulate only one radio element inside vee-validator

--- src/components/Form/RadioGroup.vue
+++ src/components/Form/RadioGroup.vue
@@ -1,11 +1,9 @@
 <template>
-  <ValidationProvider :rules="rules"
-                      :custom-messages="customMessages"
-                      #default="{errors}"
-                      tag="div">
+  <div>
     <FormInputHeader :label-for="name"
-                      :title="title"
-                      :subtitle="subtitle">
+                     :title="title"
+                     :subtitle="subtitle"
+                     :required="required">
       <template #title>
         <slot name="title"></slot>
       </template>
@@ -16,22 +14,39 @@
     <div v-for="(opt, index) in options"
          :key="index"
          class="mb-2 flex flex-row justify-start items-baseline">
-      <input type="radio"
-            :checked="opt === value"
-            :name="name"
-            class="mr-2"
-            @change="onChange(opt, $event)">
+      <template v-if="index === 0">
+        <ValidationProvider :rules="rules"
+                            :custom-messages="customMessages"
+                            ref="validator"
+                            tag="div"
+                            @hook:mounted="onValidatorMounted">
+          <input type="radio"
+                 :value="opt"
+                 :checked="opt === value"
+                 :name="name"
+                 class="mr-2"
+                 @change="onChange(opt, $event)">
+        </ValidationProvider>
+      </template>
+      <template v-else>
+        <input type="radio"
+               :value="opt"
+               :checked="opt === value"
+               :name="name"
+               class="mr-2"
+               @change="onChange(opt, $event)">
+      </template>
       <label :for="opt">
         {{opt}}
       </label>
     </div>
-    <p v-if="errors.length"
+    <!-- <p v-if="errors.length"
        class="form-input__error-hint">
       <slot name="error">
         {{errors[0]}}
       </slot>
-    </p>
-  </ValidationProvider>
+    </p> -->
+  </div>
 </template>
 
 <script>
@@ -62,7 +77,23 @@ export default {
       type: String
     }
   },
+  data () {
+    return {
+      errors: []
+    }
+  },
   methods: {
+    onValidatorMounted () {
+      this.$watch(
+        function () {
+          return this.$refs.validator.length ? this.$refs.validator[0].errors : []
+        },
+        function (errors) {
+          this.errors = errors
+        },
+        { immediate: true, deep: true }
+      )
+    },
     onChange (option, event) {
       if (event.target.checked) {
         this.$emit('change', option)
