a11adb0db27601e46b111f0e237bf44036bf8ab4
adrianpdm <adrian.padmodihardjo@gmail.com>
2020-03-12 14:51:41 +0700

    add sticky behavior to section list

--- src/views/ProfileEdit.vue
+++ src/views/ProfileEdit.vue
@@ -2,7 +2,9 @@
   <div class="messages">
     <div class="container mx-auto py-8 md:px-4">
       <div class="overflow-visible lg:overflow-hidden max-w-5xl mx-auto rounded-none lg:rounded-lg lg:shadow lg:flex lg:flex-row lg:justify-start lg:items-stretch">
-        <ProfileSectionList :sections="profileSections"
+        <ProfileSectionList @hook:mounted="keepSectionListRef"
+                            ref="sectionList"
+                            :sections="profileSections"
                             :active="activeProfileSectionName"
                             class="rounded-none sm:rounded-lg lg:rounded-none border-r border-solid border-gray-200 bg-gray-100 shadow lg:shadow-none"
                             style="flex: 1 1 25%;"
@@ -18,6 +20,8 @@
 </template>
 
 <script>
+import _debounce from 'lodash/debounce'
+import _throttle from 'lodash/throttle'
 import _startCase from 'lodash/startCase'
 import _snakeCase from 'lodash/snakeCase'
 import { mapState } from 'vuex'
@@ -43,8 +47,24 @@ export default {
         'Bank Account',
         'Emergency Contact',
         'Enneagram'
-      ]
+      ],
+      sectionListEl: null,
+      sectionListElOffsetTop: null
     }
+  },
+  mounted () {
+    this.$nextTick(() => {
+      window.removeEventListener('resize', this.onWindowResize)
+      window.removeEventListener('scroll', this.onWindowScroll)
+      setTimeout(() => {
+        window.addEventListener('resize', this.onWindowResize)
+        this.onWindowResize()
+      }, 50)
+    })
+  },
+  beforeDestroy () {
+    window.removeEventListener('resize', this.onWindowResize)
+    window.removeEventListener('scroll', this.onWindowScroll)
   },
 
   computed: {
@@ -57,6 +77,46 @@ export default {
   },
 
   methods: {
+    keepSectionListRef () {
+      const { $el } = this.$refs.sectionList
+      this.sectionListEl = $el
+      this.sectionListElOffsetTop = $el.offsetTop
+      $el.style.setProperty('will-change', 'transform')
+    },
+    stickifySectionList () {
+      window.removeEventListener('scroll', this.onWindowScroll)
+      setTimeout(() => {
+        window.addEventListener('scroll', this.onWindowScroll)
+        this.onWindowScroll()
+      }, 50)
+    },
+    unstickifySectionList () {
+      window.removeEventListener('scroll', this.onWindowScroll)
+      window.requestAnimationFrame(() => {
+        this.sectionListEl.style.setProperty(`transform`, `translateY(0px)`)
+      })
+    },
+    onWindowScroll: _throttle(function () {
+      if (!this.sectionListEl) return
+      if (window.scrollY > this.sectionListElOffsetTop) {
+        const y = Math.abs(window.scrollY - this.sectionListElOffsetTop)
+        window.requestAnimationFrame(() => {
+          this.sectionListEl.style.setProperty(`transform`, `translateY(${y}px)`)
+        })
+      } else {
+        window.requestAnimationFrame(() => {
+          this.sectionListEl.style.setProperty(`transform`, `translateY(0px)`)
+        })
+      }
+    }, Math.ceil(1000 / 144), { leading: true, trailing: false }),
+    onWindowResize: _debounce(function () {
+      const { innerWidth } = window
+      if (innerWidth >= 1024) {
+        this.stickifySectionList()
+      } else {
+        this.unstickifySectionList()
+      }
+    }, Math.ceil(1000 / 144)),
     setActiveSection (name) {
       if (name === this.activeProfileSectionName) return
       this.$router.push({
