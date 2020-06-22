8f66362a2f542e81456691a68d4dfb8b31bcbb74
adrianpdm <adrian.padmodihardjo@gmail.com>
2020-06-22 14:08:58 +0700

    package: add vue-google-oauth2; setup: set vue-google-oauth2 as vue plugin;

--- src/main.js
+++ src/main.js
@@ -3,15 +3,49 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
-import './lib/firebase'
-
-import '@fortawesome/fontawesome-free/js/all'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
-new Vue({
-  router,
-  store,
-  ...App
-})
+/**
+ * prevent chunk to be included on entry point
+ */
+function importLib () {
+  const fortAwesome = import(
+    /* webpackChunkName: "fort-awesome" */
+    /* webpackMode: "lazy" */
+    '@fortawesome/fontawesome-free/js/all'
+  )
+  return Promise.all([fortAwesome])
+}
+
+function importPlugins () {
+  const ctx = require.context('./plugins/', false, /\.js$/, 'lazy')
+  const plugins = ctx.keys().map(key => {
+    return ctx(key)
+  })
+  return Promise.all(plugins)
+}
+
+/**
+ * asynchronously load all extraneous chunks before Vue initialization
+ */
+async function init () {
+  await importLib()
+  await importPlugins()
+    .then(plugins => {
+      for (let p of plugins) {
+        if (!p || typeof p !== 'function') {
+          continue
+        }
+        Vue.use(p)
+      }
+    })
+  /* eslint-disable no-new */
+  new Vue({
+    router,
+    store,
+    ...App
+  })
+}
+
+init()
---
+++ src/plugins/vue-google-oauth2.js
@@ -0,0 +1,13 @@
+import GAuth from 'vue-google-oauth2'
+
+const opts = {
+  clientId: process.env.VUE_APP_SECRET_CLIENT_GOOGLE,
+  scope: 'profile email',
+  prompt: 'select_account'
+}
+
+export default {
+  install (Vue) {
+    Vue.use(GAuth, opts)
+  }
+}
