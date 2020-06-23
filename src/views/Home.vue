a02e2ce4217080adaf30a7f872906adf6394de72
adrianpdm <adrian.padmodihardjo@gmail.com>
2020-06-23 10:47:17 +0700

    refactor: define menu items as array

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,71 +10,22 @@
 
           <div class="mx-0 sm:mx-2 flex flex-wrap shadow bg-white">
             <template v-if="!user">
-              <div class="w-full pt-4">
-                <div class="text-center">
+              <div class="w-full pt-4 text-center">
+                <div class="">
                   <i class="far fa-4x fa-check-circle mb-4 text-gray-600" />
                   <p class="text-sm">Silahkan login untuk melakukan checkin.</p>
                 </div>
-
                 <div class="mx-2 my-2">
                   <login-button />
                 </div>
               </div>
             </template>
             <template v-else>
-              <div class="w-1/3">
-                <router-link to="/checkins">
+              <div v-for="(m, index) in menuItems" :key="index" class="w-1/3">
+                <router-link :to="m.to">
                   <div class="h-full p-3 py-4 text-center text-gray-700">
-                    <i class="far fa-check-circle text-2xl" />
-                    <p class="text-xs mt-1">Checkin</p>
-                  </div>
-                </router-link>
-              </div>
-              <div class="w-1/3">
-                <router-link to="/report">
-                  <div class="h-full p-3 py-4  text-center text-gray-700">
-                    <i class="fa fa-book text-2xl" />
-                    <p class="text-xs mt-1">Laporan</p>
-                  </div>
-                </router-link>
-              </div>
-              <div class="w-1/3">
-                <router-link to="/events">
-                  <div class="h-full p-3 py-4  text-center text-gray-700">
-                    <i class="far fa-calendar text-2xl" />
-                    <p class="text-xs mt-1">Events</p>
-                  </div>
-                </router-link>
-              </div>
-              <div class="w-1/3">
-                <router-link to="/thankyou">
-                  <div class="h-full p-3 py-4 text-center text-gray-700">
-                    <i class="far fa-heart text-2xl" />
-                    <p class="text-xs mt-1">Thank You</p>
-                  </div>
-                </router-link>
-              </div>
-              <div class="w-1/3">
-                <router-link to="/teams">
-                  <div class="h-full p-3 py-4 text-center text-gray-700">
-                    <i class="fas fa-user-friends text-2xl" />
-                    <p class="text-xs mt-1">Teams</p>
-                  </div>
-                </router-link>
-              </div>
-              <div class="w-1/3">
-                <router-link to="/booking">
-                  <div class="h-full p-3 py-4  text-center text-gray-700">
-                    <i class="far fa-building text-2xl" />
-                    <p class="text-xs mt-1">Reservasi Ruangan</p>
-                  </div>
-                </router-link>
-              </div>
-              <div class="w-1/3">
-                <router-link to="/underconstruction">
-                  <div class="h-full p-3 py-4 text-center text-gray-700">
-                    <i class="fas fa-wine-bottle text-2xl" />
-                    <p class="text-xs mt-1">Pesan Kopi</p>
+                    <i :class="[m.icon, 'text-2xl'] " />
+                    <p class="text-xs mt-1">{{m.name}}</p>
                   </div>
                 </router-link>
               </div>
@@ -107,6 +58,20 @@ export default {
 
   metaInfo: {
     title: 'Home'
+  },
+
+  data () {
+    return {
+      menuItems: [
+        { name: 'Checkin', to: '/checkins', icon: 'far fa-check-circle' },
+        { name: 'Laporan', to: '/report', icon: 'fa fa-book ' }
+        // { name: 'Events', to: '/events', icon: 'far fa-calendar ' },
+        // { name: 'Thank You', to: '/thankyou', icon: 'far fa-heart ' },
+        // { name: 'Teams', to: '/teams', icon: 'fas fa-user-friends ' },
+        // { name: 'Booking', to: '/booking', icon: 'far fa-building ' },
+        // { name: 'Pesan Kopi', to: '/underconstruction', icon: 'fas fa-wine-bottle ' }
+      ]
+    }
   },
 
   computed: mapGetters({
