b03fa1839db358fe921124676503caa8a19ce340
adrianpdm <adrian.padmodihardjo@gmail.com>
2020-06-23 19:41:18 +0700

    feat: add logbook creation button on mobile viewport;

--- src/components/LogbookList/card-list.vue
+++ src/components/LogbookList/card-list.vue
@@ -22,6 +22,15 @@
           :index="getAbsoluteIndex(index)"/>
       </ul>
     </template>
+    <div
+      class="fixed left-0 right-0 w-full flex justify-center items-center"
+      style="bottom: 5rem;">
+      <button
+        class="floating-action-button bg-blue-500"
+        @click="onCreateNewLogbook">
+        + Buat Laporan Baru
+      </button>
+    </div>
   </div>
 </template>
 
@@ -40,5 +49,17 @@ export default {
 <style lang="scss" scoped>
 .logbook-cards {
   @apply list-none;
+}
+
+.floating-action-button {
+  @apply
+  px-4 py-2 rounded-full
+  text-white
+  shadow-xl;
+
+  &:focus,
+  &:hover {
+    @apply outline-none opacity-50;
+  }
 }
 </style>
