821c66164c35cfdc66673a8de2861605039ccadb
adrianpdm <adrian.padmodihardjo@gmail.com>
2020-06-23 17:20:36 +0700

    feat: add Rating component;

---
+++ src/components/Rating.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  props: {
+    max: {
+      type: Number,
+      default: 5
+    },
+    rating: {
+      type: Number
+    }
+  },
+  render (h) {
+    const ratings = []
+    for (let i = 0; i < this.max; i++) {
+      const vnode = this.$createElement('i', {
+        key: i,
+        class: 'rating__default-symbol',
+        style: {
+          opacity: i < this.rating ? 1 : 0.33
+        }
+      })
+      ratings.push(vnode)
+    }
+    return h('div', ratings)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.rating__default-symbol {
+  @apply inline-block w-3 h-3 mr-1 rounded-full
+  bg-brand-blue-darkest;
+}
+</style>
