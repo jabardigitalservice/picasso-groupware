1f21918fa32d1ce799ca9aba7cd5acd67ef5affe
adrianpdm <adrian.padmodihardjo@gmail.com>
2020-07-06 15:48:18 +0700

    LogbookDetailAndEdit: activate detail and edit functionality

--- src/views/Logbook/Action.vue
+++ src/views/Logbook/Action.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="max-w-xl mx-auto mt-16 p-8 bg-white rounded shadow-xl">
     <h1 class="font-bold text-2xl text-green-600">
-      Buat Laporan Baru
+      {{ pageTitle }}
     </h1>
     <hr class="my-8"/>
-    <FormLogbook :action="$route.params.action" :id="$route.query.id"/>
+    <FormLogbook
+      :action="$route.params.action"
+      :id="$route.query.id"
+      :on-cancel-callback="onActionCancelled"
+      @logbook:not-found="onLogbookNotFound"/>
   </div>
 </template>
 
@@ -12,6 +16,32 @@
 export default {
   components: {
     FormLogbook: () => import('../../components/FormLogbook')
+  },
+  computed: {
+    isCreating () {
+      return this.$route.params.action === 'create'
+    },
+    isEditing () {
+      return this.$route.params.action === 'edit'
+    },
+    pageTitle () {
+      if (this.isCreating) {
+        return 'Buat Laporan Baru'
+      } else if (this.isEditing) {
+        return 'Edit Laporan'
+      }
+      return null
+    }
+  },
+  methods: {
+    onLogbookNotFound () {
+      this.$router.replace({
+        path: '/report'
+      })
+    },
+    onActionCancelled () {
+      return this.$router.back()
+    }
   },
   beforeRouteEnter (to, from, next) {
     if (!to.params.action) {
