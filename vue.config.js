81ffef1553fc9483ea4632d559377c0fe1f65781
Arif <arifboyz16@gmail.com>
2020-11-26 13:20:12 +0700

    improvement page speed

--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,24 @@
 const VueEnv = require('vue-enverywhere')
-const webpack = require('webpack')
-const path = require('path')
+// const webpack = require('webpack')
+// const path = require('path')
 // const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const PreloadWebpackPlugin = require('@vue/preload-webpack-plugin')
+// const PreloadWebpackPlugin = require('@vue/preload-webpack-plugin')
 
-let plugins = [
-  new VueEnv({
-    filename: 'vue-env.js'
-  }),
-  // new MiniCssExtractPlugin({
-  //   filename: "[name].css",
-  // }),
-  new webpack.IgnorePlugin(
-    /^\.\/locale$/, /moment$/
-  ),
-  new PreloadWebpackPlugin({
-    rel: 'preload',
-    as: 'script'
-  }),
-]
+// let plugins = [
+//   new VueEnv({
+//     filename: 'vue-env.js'
+//   }),
+//   // new MiniCssExtractPlugin({
+//   //   filename: "[name].css",
+//   // }),
+//   new webpack.IgnorePlugin(
+//     /^\.\/locale$/, /moment$/
+//   ),
+//   new PreloadWebpackPlugin({
+//     rel: 'preload',
+//     as: 'script'
+//   }),
+// ]
 
 // function resolve(dir) {
 //   return path.join(__dirname, dir)
@@ -26,6 +26,17 @@ let plugins = [
 
 module.exports = {
   configureWebpack: {
-    plugins
+    plugins: [
+      new VueEnv({
+        filename: 'vue-env.js'
+      }),
+      // new webpack.IgnorePlugin(
+      //   /^\.\/locale$/, /moment$/
+      // ),
+      // new PreloadWebpackPlugin({
+      //   rel: 'preload',
+      //   as: 'script'
+      // }),
+    ]
   },
 }
