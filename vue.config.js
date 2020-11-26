5c6e3efcf64e8c554051ab457eafcfe481bda0d2
Arif <arifboyz16@gmail.com>
2020-11-26 13:46:52 +0700

    improvement page speed

--- vue.config.js
+++ vue.config.js
@@ -1,28 +1,5 @@
 const VueEnv = require('vue-enverywhere')
-// const webpack = require('webpack')
-// const path = require('path')
-// const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-// const PreloadWebpackPlugin = require('@vue/preload-webpack-plugin')
-
-// let plugins = [
-//   new VueEnv({
-//     filename: 'vue-env.js'
-//   }),
-//   // new MiniCssExtractPlugin({
-//   //   filename: "[name].css",
-//   // }),
-//   new webpack.IgnorePlugin(
-//     /^\.\/locale$/, /moment$/
-//   ),
-//   new PreloadWebpackPlugin({
-//     rel: 'preload',
-//     as: 'script'
-//   }),
-// ]
-
-// function resolve(dir) {
-//   return path.join(__dirname, dir)
-// }
+const webpack = require('webpack')
 
 module.exports = {
   configureWebpack: {
@@ -30,13 +7,9 @@ module.exports = {
       new VueEnv({
         filename: 'vue-env.js'
       }),
-      // new webpack.IgnorePlugin(
-      //   /^\.\/locale$/, /moment$/
-      // ),
-      // new PreloadWebpackPlugin({
-      //   rel: 'preload',
-      //   as: 'script'
-      // }),
+      new webpack.IgnorePlugin(
+        /^\.\/locale$/, /moment$/
+      ),
     ]
   },
   chainWebpack: (config) => {
