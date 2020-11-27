519bedd101cc48155508eb6573ce987ae53be416
Arif <arifboyz16@gmail.com>
2020-11-27 08:51:49 +0700

    setup webpack

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const VueEnv = require('vue-enverywhere')
 const webpack = require('webpack')
+var MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   configureWebpack: {
@@ -10,6 +11,9 @@ module.exports = {
       new webpack.IgnorePlugin(
         /^\.\/locale$/, /moment$/
       ),
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+      })
     ]
   },
   chainWebpack: (config) => {
@@ -17,6 +21,28 @@ module.exports = {
     config.plugin('preload').tap((options) => {
       options[0].include = 'allChunks'
       return options
+    }),
+    config.when(process.env.NODE_ENV === 'production', config => {
+      config.plugins.delete('optimize-css')
+      config.performance
+        .maxEntrypointSize(400000)
+        .maxAssetSize(400000)
+      config.optimization.splitChunks({
+        minSize: 10000,
+        maxSize: 200000,
+        cacheGroups: {
+          default: false,
+          common: false,
+          styles: {
+              name: 'app',
+              test: /[\\/]node_modules[\\/]/,
+              // test: /\.(s?css|vue)$/, // chunks plugin has to be aware of all kind of files able to output css in order to put them together
+              chunks: 'initial',
+              minChunks: 1,
+              enforce: true
+          }
+        }
+      })
     })
   },
 }
