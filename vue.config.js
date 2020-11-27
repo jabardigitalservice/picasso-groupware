82e69335532feeaa9bc43b0b101cc9a4fd118f34
Arif <arifboyz16@gmail.com>
2020-11-27 09:48:09 +0700

    setup webpack

--- vue.config.js
+++ vue.config.js
@@ -14,7 +14,27 @@ module.exports = {
       new MiniCssExtractPlugin({
         filename: '[name].css',
       })
-    ]
+    ],
+    optimization: {
+      runtimeChunk: "single",
+      splitChunks: {
+        chunks: "all",
+        maxInitialRequests: Infinity,
+        minSize: 0,
+        maxSize: 200000,
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name(module) {
+              const packageName = module.context.match(
+                /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+              )[1];
+              return `npm.${packageName.replace("@", "")}`;
+            }
+          }
+        }
+      }
+    }
   },
   chainWebpack: (config) => {
     config.plugins.delete('prefetch')
@@ -22,31 +42,34 @@ module.exports = {
       options[0].include = 'allChunks'
       return options
     }),
+    config.module
+      .rule('vue')
+      .type("javascript/auto")
+      .use('vue-loader')
+      .loader('vue-loader')
+      .end()
+    config.performance.maxEntrypointSize(400000).maxAssetSize(400000)
     config.when(process.env.NODE_ENV === 'production', config => {
-      config.plugins.delete('optimize-css')
-      config.performance
-        .maxEntrypointSize(400000)
-        .maxAssetSize(400000)
-      config.optimization.runtimeChunk('single')
-      config.optimization.splitChunks({
-        chunks: 'all',
-        maxInitialRequests: Infinity,
-        minSize: 100000,
-        maxSize: 200000,
-        cacheGroups: {
-          default: false,
-          common: false,
-          styles: {
-              name: 'app',
-              test: /[\\/]node_modules[\\/]/,
-              // test: /\.(s?css|vue)$/, // chunks plugin has to be aware of all kind of files able to output css in order to put them together
-              name(module) {
-                const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-                return `npm.${packageName.replace('@', '')}`;
-              },
-          }
-        }
-      })
+    config.plugins.delete('optimize-css')
+      // config.optimization.splitChunks({
+      //   chunks: 'all',
+      //   maxInitialRequests: Infinity,
+      //   minSize: 100000,
+      //   maxSize: 200000,
+      //   cacheGroups: {
+      //     default: false,
+      //     common: false,
+      //     styles: {
+      //         name: 'app',
+      //         test: /[\\/]node_modules[\\/]/,
+      //         // test: /\.(s?css|vue)$/, // chunks plugin has to be aware of all kind of files able to output css in order to put them together
+      //         name(module) {
+      //           const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+      //           return `npm.${packageName.replace('@', '')}`;
+      //         },
+      //     }
+      //   }
+      // })
       config.optimization.minimizer("terser").tap(args => {
         const { terserOptions } = args[0]
         terserOptions.compress = true
