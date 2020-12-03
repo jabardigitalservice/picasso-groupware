847739aa9cccc289588fefe3be8195909b824e45
Arif <arifboyz16@gmail.com>
2020-12-03 15:39:48 +0700

    bugs in request firebase to get profile

--- src/views/ProfileDetail.vue
+++ src/views/ProfileDetail.vue
@@ -3,82 +3,74 @@
     <div class="container max-w-5xl mx-auto">
       <DataCompletionStatus mode="redirect"
                             class="w-full md:mb-8 mx-auto bg-transparent md:bg-gray-300"/>
-      <template v-if="user && !loading">
-        <template v-if="!error">
-          <div class="bg-white m-0 mt-24 lg:mt-32 sm:rounded shadow">
-            <div class="flex mx-auto justify-center absolute inset-x-0 photo-center">
-              <a :href="user.photo">
-                <img class="transform rotate-0 h-24 w-24 lg:h-32 lg:w-32 rounded-full mx-auto" :src="user.photo" :alt="user.fullname" />
-              </a>
-            </div>
-            <div class="text-center p-6 border-b">
-              <p class="pt-4 text-lg font-semibold">
-                {{ user.fullname }}
-              </p>
-              <p v-if="user.jabatan" class="text-gray-600">
-                {{ user.jabatan }}
-              </p>
-            </div>
-            <div class="w-full p-2">
-              <AttendanceCardUser />
-            </div>
-            <!-- <div class="border-b">
-              <a href="#" class="px-2 py-3 hover:bg-gray-200 flex">
-                <div class="pl-3">
-                  <p class="font-semibold">
-                    Email
-                  </p>
-                  <p class="text-gray-600">
-                    {{ item['email'] ? item['email'] : '-' }}
-                  </p>
-                </div>
-              </a>
-              <a href="#" class="px-2 py-3 hover:bg-gray-200 flex">
-                <div class="pl-3">
-                  <p class="font-semibold">
-                    Phone
-                  </p>
-                  <p class="text-gray-600">
-                    {{ item['phone'] ? item['phone'] : '-' }}
-                  </p>
-                </div>
-              </a>
-              <a href="#" class="px-2 py-3 hover:bg-gray-200 flex">
-                <div class="pl-3">
-                  <p class="font-semibold">
-                    Birth Date
-                  </p>
-                  <p class="text-gray-600">
-                    {{ item['birthdate'] ? formatDateLong(item['birthdate'].toDate()) : '-' }}
-                  </p>
-                </div>
-              </a>
-              <a href="#" class="px-2 py-3 hover:bg-gray-200 flex">
-                <div class="pl-3">
-                  <p class="font-semibold">
-                    Last Online
-                  </p>
-                  <p class="text-gray-600">
-                    {{ formatDateTimeShort(item['last_seen_at'].toDate()) }}
-                  </p>
-                </div>
-              </a>
-            </div> -->
-            <!-- <div class="border-b">
-              <div class="px-4 py-4 text-center">
-                <router-link to="/profile/edit/personal" class="w-full text-center shadow block bg-brand-blue text-white font-bold py-2 px-4 rounded">
-                  Edit Profile
-                </router-link>
+      <template v-if="user">
+        <div class="bg-white m-0 mt-24 lg:mt-32 sm:rounded shadow">
+          <div class="flex mx-auto justify-center absolute inset-x-0 photo-center">
+            <a :href="user.photo">
+              <img class="transform rotate-0 h-24 w-24 lg:h-32 lg:w-32 rounded-full mx-auto" :src="user.photo" :alt="user.fullname" />
+            </a>
+          </div>
+          <div class="text-center p-6 border-b">
+            <p class="pt-4 text-lg font-semibold">
+              {{ user.fullname }}
+            </p>
+            <p v-if="user.jabatan" class="text-gray-600">
+              {{ user.jabatan }}
+            </p>
+          </div>
+          <div class="w-full p-2">
+            <AttendanceCardUser />
+          </div>
+          <!-- <div class="border-b">
+            <a href="#" class="px-2 py-3 hover:bg-gray-200 flex">
+              <div class="pl-3">
+                <p class="font-semibold">
+                  Email
+                </p>
+                <p class="text-gray-600">
+                  {{ item['email'] ? item['email'] : '-' }}
+                </p>
               </div>
-            </div> -->
-          </div>
-        </template>
-        <template v-else>
-          <div class="bg-red-100 border border-red-400 text-red-700 mx-2 sm:mx-0 px-4 py-3 relative" role="alert">
-            <strong class="font-bold">Holy smokes! </strong>
-            <span class="block sm:inline">Something seriously bad happened.</span>
-          </div>
-        </template>
+            </a>
+            <a href="#" class="px-2 py-3 hover:bg-gray-200 flex">
+              <div class="pl-3">
+                <p class="font-semibold">
+                  Phone
+                </p>
+                <p class="text-gray-600">
+                  {{ item['phone'] ? item['phone'] : '-' }}
+                </p>
+              </div>
+            </a>
+            <a href="#" class="px-2 py-3 hover:bg-gray-200 flex">
+              <div class="pl-3">
+                <p class="font-semibold">
+                  Birth Date
+                </p>
+                <p class="text-gray-600">
+                  {{ item['birthdate'] ? formatDateLong(item['birthdate'].toDate()) : '-' }}
+                </p>
+              </div>
+            </a>
+            <a href="#" class="px-2 py-3 hover:bg-gray-200 flex">
+              <div class="pl-3">
+                <p class="font-semibold">
+                  Last Online
+                </p>
+                <p class="text-gray-600">
+                  {{ formatDateTimeShort(item['last_seen_at'].toDate()) }}
+                </p>
+              </div>
+            </a>
+          </div> -->
+          <!-- <div class="border-b">
+            <div class="px-4 py-4 text-center">
+              <router-link to="/profile/edit/personal" class="w-full text-center shadow block bg-brand-blue text-white font-bold py-2 px-4 rounded">
+                Edit Profile
+              </router-link>
+            </div>
+          </div> -->
+        </div>
       </template>
       <template v-else>
         <div class="bg-white shadow p-4">
@@ -100,7 +92,6 @@
 import { mapGetters } from 'vuex'
 import { ContentLoader } from 'vue-content-loader'
 import { formatDateTimeShort, formatDateLong } from '@/lib/date'
-import { analytics } from '@/lib/firebase'
 import AttendanceCardUser from '@/components/Dashboard/AttendanceCardUser'
 
 export default {
@@ -117,27 +108,12 @@ export default {
   },
 
   computed: mapGetters({
-    user: 'auth/user',
-    loading: 'profile-detail/loading',
-    item: 'profile-detail/item',
-    error: 'profile-detail/error'
+    user: 'auth/user'
   }),
-
-  mounted () {
-    this.fetchItem()
-  },
 
   methods: {
     formatDateTimeShort,
-    formatDateLong,
-
-    async fetchItem () {
-      await this.$store.dispatch('profile-detail/fetchItem', {
-        id: this.user.id
-      })
-
-      analytics.logEvent('profile_detail_view')
-    }
+    formatDateLong
   }
 }
 </script>
