5ba955317c97f32ba50b20b2960c931a4acf54c4
adrianpdm <adrian.padmodihardjo@gmail.com>
2021-01-25 23:04:40 +0700

    add "InputTupoksi" form input component;

---
+++ src/components/FormLogbook/InputTupoksi.vue
@@ -0,0 +1,96 @@
+<template>
+  <ValidationProvider
+    rules="required"
+    :custom-messages="{
+      required: 'Uraian tugas harus diisi'
+    }"
+    #default="{errors}"
+    tag="div"
+  >
+    <FormInputHeader
+      :label-for="name"
+      :title="inputTitle"
+      :required="isRequired"
+    />
+    <FormInputRadioGroup
+      :name="name"
+      :options="listOfTupoksi"
+      value-key="id"
+      label-key="name_tupoksi"
+      :value="inputValue"
+      :show-as-readonly-input="showAsReadonlyInput"
+      @change="onValueChanged"
+    >
+    </FormInputRadioGroup>
+    <p v-if="errors.length"
+       class="form-input__error-hint">
+      <slot name="error">
+        {{errors[0]}}
+      </slot>
+    </p>
+  </ValidationProvider>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import FormInputHeader from '../Form//InputHeader'
+import FormInputRadioGroup from '../Form/RadioGroup.vue'
+export default {
+  components: {
+    FormInputHeader,
+    FormInputRadioGroup
+  },
+  model: {
+    prop: 'value',
+    event: 'change'
+  },
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    value: {
+      type: [String, Number],
+      default: ''
+    },
+    showAsReadonlyInput: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data () {
+    return {
+      isRequired: true,
+      inputTitle: 'Uraian Tugas',
+      inputValue: null
+    }
+  },
+  computed: {
+    ...mapState('organizations', [
+      'listOfTupoksi'
+    ])
+  },
+  watch: {
+    value: {
+      immediate: true,
+      handler (newVal, oldVal) {
+        if (newVal !== oldVal) {
+          this.inputValue = newVal
+        }
+      }
+    }
+  },
+  created () {
+    this.$store.dispatch('organizations/fetchListOfTupoksi')
+  },
+  methods: {
+    onValueChanged (newVal) {
+      this.$emit('change', newVal)
+    }
+  }
+}
+</script>
