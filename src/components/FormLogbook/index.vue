13c001cdcb344bbfd0ee82c51b5de7fe1f3ee0f7
adrianpdm <adrian.padmodihardjo@gmail.com>
2021-01-25 23:05:21 +0700

    consume "InputTupoksi" component inside FormLogbook;

--- src/components/FormLogbook/index.vue
+++ src/components/FormLogbook/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     <ValidationObserver #default="{handleSubmit}">
+      <FormInputTupoksi
+        name="tupoksiJabatanId"
+        :show-as-readonly-input="isViewingOnly"
+        :disabled="!isEditable"
+        v-model="payload.tupoksiJabatanId" />
+      <br />
       <FormInputProject
         name="projectId"
         title="Nama Proyek"
@@ -238,8 +244,8 @@
 </template>
 
 <script>
+import FormInputTupoksi from './InputTupoksi'
 import FormInputProject from './InputProjectAutocomplete'
-// import FormSelect from '../Form/Select'
 import FormInput from '../Form/Input'
 import FormInputEvidence from '../Form/EvidenceImageInput'
 import FormInputDocument from '../Form/DocumentFileInput'
@@ -261,7 +267,8 @@ const modelData = {
   'organizerTask': null, // ?,
   'isMainTask': null,
   'isDocumentLink': true,
-  'workPlace': null
+  'workPlace': null,
+  'tupoksiJabatanId': null
 }
 
 const ACTION = {
@@ -278,7 +285,7 @@ const ACTIONS = [
 
 export default {
   components: {
-    // FormSelect,
+    FormInputTupoksi,
     FormInputProject,
     FormInput,
     FormInputEvidence,
