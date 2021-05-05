b41b029bbb14c75d90b39ec15dde206df3f04ca9
adrianpdm <adrian.padmodihardjo@gmail.com>
2021-05-05 18:26:36 +0700

    make special wrapper for logged in user profpic;

---
+++ src/components/Profile/UserProfilePicture.vue
@@ -0,0 +1,27 @@
+<template>
+  <ProfilePicture
+    :size="size"
+    :url="user ? user.photo : null"
+    :is-loading="isLoading" />
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import ProfilePicture from './ProfilePicture.vue'
+
+export default {
+  components: {
+    ProfilePicture
+  },
+  props: {
+    size: {
+      type: Number,
+      default: 64
+    }
+  },
+  computed: mapGetters({
+    isLoading: 'auth/loading',
+    user: 'auth/user'
+  })
+}
+</script>
