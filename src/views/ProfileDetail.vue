85bc093cd2b388b9df4069523495f84e04bb4f58
adrianpdm <adrian.padmodihardjo@gmail.com>
2021-05-05 18:27:02 +0700

    remove unused code; refactor profpic;

--- src/views/ProfileDetail.vue
+++ src/views/ProfileDetail.vue
@@ -1,20 +1,14 @@
 <template>
   <div class="messages">
     <div class="container max-w-5xl mx-auto">
-      <DataCompletionStatus mode="redirect"
-                            class="w-full md:mb-8 mx-auto bg-transparent md:bg-gray-300"/>
       <template v-if="user">
-        <div class="bg-white m-0 mt-24 lg:mt-32 sm:rounded shadow">
-          <div class="flex mx-auto justify-center absolute inset-x-0 photo-center">
-            <a :href="user.photo || null">
-              <img
-                class="transform rotate-0 h-24 w-24 lg:h-32 lg:w-32 rounded-full mx-auto"
-                :src="user.photo || AVATAR_PLACEHOLDER"
-                :alt="user.fullname" />
-            </a>
-          </div>
-          <div class="text-center p-6 border-b">
-            <p class="pt-4 text-lg font-semibold">
+        <div class="flex flex-wrap justify-center m-0 mt-24 lg:mt-32">
+          <UserProfilePicture
+            class="absolute transform -translate-y-16"
+            :size="128" />
+          <br>
+          <div class="bg-white flex-none w-full text-center p-6 pt-20 sm:rounded shadow">
+            <p class="text-lg font-semibold">
               {{ user.fullname }}
             </p>
             <p v-if="user.jabatan" class="text-gray-600">
@@ -24,55 +18,6 @@
           <div class="w-full p-2">
             <AttendanceCardUser />
           </div>
-          <!-- <div class="border-b">
-            <a href="#" class="px-2 py-3 hover:bg-gray-200 flex">
-              <div class="pl-3">
-                <p class="font-semibold">
-                  Email
-                </p>
-                <p class="text-gray-600">
-                  {{ item['email'] ? item['email'] : '-' }}
-                </p>
-              </div>
-            </a>
-            <a href="#" class="px-2 py-3 hover:bg-gray-200 flex">
-              <div class="pl-3">
-                <p class="font-semibold">
-                  Phone
-                </p>
-                <p class="text-gray-600">
-                  {{ item['phone'] ? item['phone'] : '-' }}
-                </p>
-              </div>
-            </a>
-            <a href="#" class="px-2 py-3 hover:bg-gray-200 flex">
-              <div class="pl-3">
-                <p class="font-semibold">
-                  Birth Date
-                </p>
-                <p class="text-gray-600">
-                  {{ item['birthdate'] ? formatDateLong(item['birthdate'].toDate()) : '-' }}
-                </p>
-              </div>
-            </a>
-            <a href="#" class="px-2 py-3 hover:bg-gray-200 flex">
-              <div class="pl-3">
-                <p class="font-semibold">
-                  Last Online
-                </p>
-                <p class="text-gray-600">
-                  {{ formatDateTimeShort(item['last_seen_at'].toDate()) }}
-                </p>
-              </div>
-            </a>
-          </div> -->
-          <!-- <div class="border-b">
-            <div class="px-4 py-4 text-center">
-              <router-link to="/profile/edit/personal" class="w-full text-center shadow block bg-brand-blue text-white font-bold py-2 px-4 rounded">
-                Edit Profile
-              </router-link>
-            </div>
-          </div> -->
         </div>
       </template>
       <template v-else>
@@ -97,6 +42,7 @@ import { ContentLoader } from 'vue-content-loader'
 import { ASSETS } from '@/lib/constants'
 import { formatDateTimeShort, formatDateLong } from '@/lib/date'
 import AttendanceCardUser from '@/components/Dashboard/AttendanceCardUser'
+import UserProfilePicture from '../components/Profile/UserProfilePicture.vue'
 
 export default {
   middleware: 'check-auth',
@@ -104,7 +50,7 @@ export default {
   components: {
     ContentLoader,
     AttendanceCardUser,
-    DataCompletionStatus: () => import('../components/Profile/Edit/DataCompletionStatus')
+    UserProfilePicture
   },
 
   metaInfo: {
@@ -127,21 +73,3 @@ export default {
   }
 }
 </script>
-<style scoped>
-  .photo-center {
-    margin: -58px auto 0;
-    overflow: hidden;
-  }
-  @media only screen and (min-width: 1024px) {
-    .photo-center {
-      margin: -90px auto 0;
-      overflow: hidden;
-    }
-  }
-  @media only screen and (min-width: 1200px) {
-    .photo-center {
-      margin: -90px auto 0;
-      overflow: hidden;
-    }
-  }
-</style>
