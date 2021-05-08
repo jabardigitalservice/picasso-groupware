e7e5474c71f0c676087d2b7d3c6c403da39668d7
adrianpdm <adrian.padmodihardjo@gmail.com>
2021-05-08 20:47:15 +0700

    remove unused export

--- src/components/Reactions/index.js
+++ src/components/Reactions/index.js
@@ -1,19 +1,13 @@
 import Angry from './Variants/Angry.vue'
 import Flat from './Variants/Flat.vue'
 import Haha from './Variants/Haha.vue'
-import Like from './Variants/Like.vue'
-import Love from './Variants/Love.vue'
 import Sad from './Variants/Sad.vue'
-import Wow from './Variants/Wow.vue'
 import Yay from './Variants/Yay.vue'
 
-export {
-  Angry,
-  Flat,
-  Haha,
-  Like,
-  Love,
-  Sad,
-  Wow,
-  Yay
+export const moodMap = {
+  worst: Angry,
+  sad: Sad,
+  neutral: Flat,
+  good: Haha,
+  excellent: Yay
 }
