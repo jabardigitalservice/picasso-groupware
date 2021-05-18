a40f33e78dddd5006f2bbe4560d433f2930e4c0c
adrianpdm <adrian.padmodihardjo@gmail.com>
2021-05-18 15:55:57 +0700

    define props on component-loader render function

--- src/components/ResponsiveComponentLoader/index.js
+++ src/components/ResponsiveComponentLoader/index.js
@@ -65,7 +65,9 @@ export default {
   },
   render (h) {
     if (this.component) {
-      return h(this.component)
+      return h(this.component, {
+        props: this.$attrs || {}
+      })
     }
     return null
   }
