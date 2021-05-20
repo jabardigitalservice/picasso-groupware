855e0b69a0dbb128a3fd448e3182ddfe534780d3
adrianpdm <adrian.padmodihardjo@gmail.com>
2021-05-20 10:56:26 +0700

    make logbook table syncable with route

--- src/components/LogbookList/table.vue
+++ src/components/LogbookList/table.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <DateRangePicker @change-date="onDateChanged"/>
+    <DateRangePicker
+      :range="dateRange"
+      @change-date="onDateChanged"/>
       <div class="v-pagination-container">
         <VPagination v-bind="pagination" />
       </div>
@@ -34,7 +36,7 @@
           </thead>
           <tbody>
             <template v-if="isLoadingLogbookList">
-              <tr v-for="i in perPage" :key="`skeleton:${i}`">
+              <tr v-for="i in mQuery.perPage" :key="`skeleton:${i}`">
                 <td v-for="j in 8" :key="`col:${j}`" class="p-4">
                   <div class="w-full h-2 rounded-full bg-shimmering" />
                 </td>
@@ -92,22 +94,34 @@ export default {
       logbookListData: null,
       totalPage: 0,
       totalCount: 0,
-      page: 1,
-      perPage: 10,
-      startDate: null,
-      endDate: null
+
+      mQuery: {
+        page: 1,
+        perPage: 10,
+        startDate: null,
+        endDate: null
+      }
     }
   },
   computed: {
+    dateRange () {
+      const { startDate, endDate } = this.mQuery
+      const start = startDate ? new Date(startDate) : null
+      const end = endDate ? new Date(endDate) : null
+      return {
+        start,
+        end
+      }
+    },
     startIndex () {
       if (this.totalCount) {
-        return (this.page - 1) * (this.perPage)
+        return (this.mQuery.page - 1) * (this.mQuery.perPage)
       }
       return 0
     },
     endIndex () {
       if (this.totalCount) {
-        if (this.page === Math.ceil(this.totalCount / this.perPage)) {
+        if (this.mQuery.page === Math.ceil(this.totalCount / this.mQuery.perPage)) {
           return this.totalCount - 1
         } else {
           return this.startIndex + this.logbookListData.length - 1
@@ -117,7 +131,7 @@ export default {
     },
     pagination () {
       return {
-        value: this.page,
+        value: this.mQuery.page,
         pageCount: this.totalPage,
         pageRange: 3,
         containerClass: 'v-pagination',
@@ -140,36 +154,69 @@ export default {
         }
 
         const {
+          page = 1,
+          perPage = 10,
+          startDate = null,
+          endDate = null
+        } = (newObject || {})
+        this.updateQuery({
           page,
           perPage,
-          start_date: startDate,
-          end_date: endDate
-        } = (newObject || {})
-        this.page = page || 1
-        this.perPage = perPage || 10
-        this.startDate = startDate
-        this.endDate = endDate
+          startDate,
+          endDate
+        }, {
+          sync: false
+        })
         this.loadData()
       }
     }
   },
   methods: {
+    updateQuery ({
+      page,
+      perPage,
+      startDate,
+      endDate
+    }, {
+      sync = true
+    } = {}) {
+      this.mQuery = {
+        page,
+        perPage,
+        startDate,
+        endDate
+      }
+      if (sync) {
+        this.$emit('update:query', this.mQuery)
+      }
+    },
+    getQueryAsAPISpec () {
+      const { page, perPage, startDate, endDate } = this.mQuery
+      return {
+        page,
+        pageSize: perPage,
+        start_date: startDate,
+        end_date: endDate
+      }
+    },
     onDateChanged ({ start_date: startDate, end_date: endDate }) {
-      this.page = 1
+      this.updateQuery({
+        perPage: this.mQuery.perPage,
+        page: 1,
+        startDate,
+        endDate
+      })
       this.totalCount = 0
       this.totalPage = 0
-      this.startDate = startDate
-      this.endDate = endDate
-      this.loadData()
     },
     onPageChanged (newPage) {
-      this.page = newPage
-      this.loadData()
+      this.updateQuery({
+        ...this.mQuery,
+        page: newPage
+      })
     },
     onLoadSuccess ({ results, _meta }) {
-      const { currentPage, perPage, totalPage, totalCount } = _meta
-      this.page = currentPage
-      this.perPage = perPage
+      const { totalPage, totalCount } = _meta
       this.totalPage = totalPage
       this.totalCount = totalCount
       this.logbookListData = results
@@ -184,12 +231,9 @@ export default {
       this.isLoadingLogbookList = true
       this.logbookListError = null
 
-      this.$store.dispatch('logbook-list/getLogbookList', {
-        page: this.page,
-        pageSize: this.perPage,
-        start_date: this.startDate,
-        end_date: this.endDate
-      }).then(this.onLoadSuccess)
+      const params = this.getQueryAsAPISpec()
+      this.$store.dispatch('logbook-list/getLogbookList', params)
+        .then(this.onLoadSuccess)
         .catch(this.onLoadError)
         .finally(this.onLoadFinished)
     }
