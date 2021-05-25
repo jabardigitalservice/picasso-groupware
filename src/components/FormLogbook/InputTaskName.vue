e7d3867dae9dff99c8385cfa2010eb3239d480da
adrianpdm <adrian.padmodihardjo@gmail.com>
2021-05-25 09:36:26 +0700

    refactor input task name

---
+++ src/components/FormLogbook/InputTaskName.vue
@@ -0,0 +1,29 @@
+<template>
+  <FormInput
+    name="nameTask"
+    title="Nama Task"
+    type="text"
+    :disabled="disabled"
+    rules="required"
+    :custom-messages="{
+      required: 'Nama task harus diisi'
+    }"
+    :value="value"
+    @input="$emit('input', $event)"
+  />
+</template>
+
+<script>
+import { props } from '../Form/input-mixin'
+export default {
+  components: {
+    FormInput: () => import('../Form/Input')
+  },
+  props: {
+    ...props,
+    value: {
+      type: String
+    }
+  }
+}
+</script>
